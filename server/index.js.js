a9c7ea376d0137fe524f996ba00d8075b360a154
Dhaval Rupapara <rupaparadhaval1234@gmail.com>
2024-03-26 15:40:04 +0530

    deploy fixed

--- server/index.js.js
+++ server/index.js.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 
 const io = new Server(server, { 
   cors: {
-    origin: "*",
+    origin: ["https://code-connect-a132.vercel.app/"],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -18,7 +18,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://code-connect-a132.vercel.app/"],
     methods: ["GET", "POST"],
     credentials: true,
   })
